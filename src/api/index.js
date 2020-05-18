8c1496bc5f4e4ac9efeaa74271c47be302dd0294
ChandrashekarVT <chandrashekarvt761@gmail.com>
2020-05-18 20:35:58 +0530

    Inital Commit

---
+++ src/api/index.js
@@ -0,0 +1,52 @@
+import axios from 'axios'
+
+const url = 'https://covid19.mathdro.id/api';
+
+
+export const fetchData = async (country)=>{
+    let changeableUrl = url;
+
+    if(country){
+        changeableUrl = `${url}/countries/${country}`
+    }
+    try{
+        const { data:{confirmed, recovered, deaths, lastUpdate} } = await axios.get(changeableUrl);
+        // return response.data; instead of this
+        const modifiedData = { confirmed, recovered, deaths, lastUpdate}
+
+        return modifiedData;
+
+    }catch(error){
+        console.log(error)
+    }
+}
+
+//For charts
+
+
+export const fetchDailyData = async ()=>{
+    try {
+        const {data} = await axios.get(`${url}/daily`);
+        const modifiedData = data.map((dailyData) =>({
+            confirmed:dailyData.confirmed.total,
+            deaths :dailyData.deaths.total,
+            date:dailyData.reportDate,
+        }));
+
+        return modifiedData;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//For CountryPicker
+
+export const fetchCountries = async ()=>{
+    try {
+        const {data:{countries}} = await axios.get(`${url}/countries`);
+        return countries.map((country)=>country.name);
+
+    } catch (error) {
+        console.log(error)
+    }
+}
